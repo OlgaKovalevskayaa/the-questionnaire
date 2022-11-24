02439cf78e8893a9e86edb90b1f40794d78842d1
OlgaKovalevskayaa <krishtaoivicho@mail.ru>
2022-11-24 16:33:07 +0300

    Сделала POST-запрос

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
 
-  // Здесь твой код
+const nameee = document.querySelector("#name");
+const nameText = nameee.value;
+
+const secondName = document.querySelector("#secondName");
+const secondNameText = secondName.value;
+
+const email = document.querySelector("#email");
+const emailText = email.value;
+
+const phone = document.querySelector("#phone");
+const phoneText = phone.value;
+
+const agree = document.querySelector("#agree").checed;
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Здесь твой код
+    fetch('https://polinashneider.space/user', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: OlgaKovalevskayaa'
+            },
+            body: JSON.stringify({
+                "name": `${nameText}`,
+                "secondName": `${secondNameText}`,
+                "phone": `${emailText}`,
+                "email": `${phoneText}`,
+                "agree": `${agree}`
+            }),
+        })
+        .then((result) => {
+            return result.json();
+        })
+        .then((data) => {
+            console.log(data)
+        })
+
 });
